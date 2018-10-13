42e63815f3d2423a3bb33a5470cdc4864b81d7c0
coffincw <coffincw@gmail.com>
2018-10-13 11:02:05 -0400

    menu bar and add exam
    adjusted the menu bar and added the add exam functionality.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu} = require('electron')
 const path = require('path');
 const url = require('url');
 
 let win;
+let addWindow;
   
   function createWindow () {
     // Create the browser window.
@@ -18,16 +19,56 @@ let win;
     	slashes: true
     }));
 
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    Menu.setApplicationMenu(mainMenu);
+
     //Open devtools
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 
     win.on('closed', () => {
     	win = null;
     })
   }
   
+
+
+  const mainMenuTemplate = [
+  {
+  	label: 'File',
+  	submenu:[
+  		{
+  			label: 'Add New Exam',
+  			click(){
+  				createAddWindow();
+  			}
+  		},
+  	{
+  		label: 'Clear Items'
+  	},
+  	{
+  		label: 'Quit',
+  		accelerator: process.platform == 'darwin' ? 'Command+Q' :
+  		'Ctrl+Q',
+  		click() {
+  			app.quit();
+  		}
+  	}
+  	]
+  }
+  ];
   //create window
   app.on('ready', createWindow)
+
+  function createAddWindow() {
+	addWindow = new BrowserWindow({ width: 200, height: 300, title: 'Add New Exam' })
+	addWindow.loadURL(url.format({
+    	pathname: path.join(__dirname, 'AddExam.html'),
+    	protocol: 'file:',
+    	slashes: true
+    }));
+
+  }
 
   //quit when all windows are closed
   app.on('window-all-closed', () => {
