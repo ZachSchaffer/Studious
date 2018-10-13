e2b87afaa4eee6226f568125b6ef9f4b9bf64ac2
coffincw <coffincw@gmail.com>
2018-10-13 16:39:11 -0400

    button to second page

--- src/main.js
+++ src/main.js
@@ -7,22 +7,18 @@ let pdfUploadWindow;
   
 function createWindow () {
   // Create the browser window.
-	win = new BrowserWindow({ width: 800, height: 600 })
+	win = new BrowserWindow({ width: 800, height: 600 });
   	var python = require('child_process').spawn('python',['./helloworld.py']);
   	python.stdout.on('data', function(data) {
   		console.log("data: ", data.toString('utf8'));
   	})
 
     //and load the index.html of the app.
-    win.loadURL(url.format({
-    	pathname: path.join(__dirname, 'index.html'),
-    	protocol: 'file:',
-    	slashes: true
-    }));
+    win.loadFile('index.html');
 
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
-    Menu.setApplicationMenu(mainMenu);
+    // Menu.setApplicationMenu(mainMenu);
 
     //Open devtools
     // win.webContents.openDevTools();
@@ -41,6 +37,7 @@ const mainMenuTemplate = [
   		{
   			label: 'Open Past Study Plan',
   			click(){
+          switchToPDFUploadWindow();
   				//load study plan
   			}
   		},
@@ -65,16 +62,11 @@ const mainMenuTemplate = [
 	app.on('ready', createWindow)
 
 	function switchToPDFUploadWindow() {
-		pdfUploadWindow = win;
-		pdfUploadWindow.loadURL(url.format({
-    		pathname: path.join(__dirname, 'AddExam.html'),
-    		protocol: 'file:',
-    		slashes: true
-	}));
+    win.loadFile('AddExam.html');
 
     //garbage collection
-	pdfUploadWindow.on('close', function() {
-   		pdfUploadWindow = null;
+	win.on('close', function() {
+   		win = null;
 	});
 
 }
---
+++ src/ui elements/material ui buttons/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/kgrittner/pen/BNGqgQ.
+
+ Displays the different kinds of buttons available in Google's new MDL Light
